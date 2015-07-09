9e74548aebfd7fe7391da8cf09786732901ea06a
Tyler Schrock <Tschrock123@gmail.com>
2015-07-09 15:36:39 -0400

    TreeView Stuff

---
+++ RockWeb/Plugins/org_newpointe/Checkin/CheckinGroupList.ascx.cs
@@ -0,0 +1,273 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Attribute;
+using System.Text;
+
+namespace RockWeb.Plugins.org_newpointe.Checkin
+{
+    /// <summary>
+    /// Template block for developers to use to start a new block.
+    /// </summary>
+    [DisplayName("Check-in Group List")]
+    [Category("Check-in")]
+    [Description("Lists checkin areas and their groups based off a parent checkin configuration group type.")]
+    [GroupTypeField("Check-inType", required: false, key: "GroupTypeTemplate", groupTypePurposeValueGuid: Rock.SystemGuid.DefinedValue.GROUPTYPE_PURPOSE_CHECKIN_TEMPLATE)]
+    [LinkedPage("Group Detail Page", "Link to the group details page", false)]
+    public partial class CheckinGroupList : Rock.Web.UI.RockBlock
+    {
+        #region Fields
+
+        /// <summary>
+        /// The RestParams (used by the Markup)
+        /// </summary>
+        protected string RestParms;
+
+        /// <summary>
+        /// Gets or sets the selected category identifier.
+        /// </summary>
+        /// <value>
+        /// The selected category identifier.
+        /// </value>
+        protected int? SelectedCategoryId { get; set; }
+
+        // used for private variables
+        StringBuilder _content = new StringBuilder();
+        StringBuilder _content2 = new StringBuilder();
+        RockContext _rockContext = null;
+        #endregion
+
+        #region Properties
+
+        // used for public / protected properties
+
+        #endregion
+
+        #region Base Control Methods
+
+        //  overrides of the base RockBlock methods (i.e. OnInit, OnLoad)
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger(upnlContent);
+
+            _rockContext = new RockContext();
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!Page.IsPostBack)
+            {
+                ShowContent();
+            }
+        }
+
+
+
+        #endregion
+
+        #region Events
+
+        // handlers called by the controls on your block
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated(object sender, EventArgs e)
+        {
+            ShowContent();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private GroupType GetRootGroupType(int groupId)
+        {
+            List<int> parentRecursionHistory = new List<int>();
+            GroupTypeService groupTypeService = new GroupTypeService(_rockContext);
+            var groupType = groupTypeService.Queryable().AsNoTracking().Include(t => t.ParentGroupTypes).Where(t => t.Groups.Select(g => g.Id).Contains(groupId)).FirstOrDefault();
+
+            while (groupType != null && groupType.ParentGroupTypes.Count != 0)
+            {
+                if (parentRecursionHistory.Contains(groupType.Id))
+                {
+                    var exception = new Exception("Infinite Recursion detected in GetRootGroupType for groupId: " + groupId.ToString());
+                    LogException(exception);
+                    return null;
+                }
+                else
+                {
+                    var parentGroupType = GetParentGroupType(groupType);
+                    if (parentGroupType != null && parentGroupType.Id == groupType.Id)
+                    {
+                        // the group type's parent is itself
+                        return groupType;
+                    }
+
+                    groupType = parentGroupType;
+                }
+
+                parentRecursionHistory.Add(groupType.Id);
+            }
+
+            return groupType;
+        }
+
+        private GroupType GetParentGroupType(GroupType groupType)
+        {
+            GroupTypeService groupTypeService = new GroupTypeService(_rockContext);
+            return groupTypeService.Queryable()
+                                .Include(t => t.ParentGroupTypes)
+                                .AsNoTracking()
+                                .Where(t => t.ChildGroupTypes.Select(p => p.Id).Contains(groupType.Id)).FirstOrDefault();
+        }
+
+        private void ShowContent()
+        {
+            Guid configurationTemplateGuid = Guid.Empty;
+
+            hfSelectedItemId.Value = Request["GroupTypeId"];
+            hfInitialCategoryParentIds.Value = Server.UrlDecode(Request["ExpandedIds"]);
+
+            if (Request["Area"] != null)
+            {
+                var groupType = Rock.Web.Cache.GroupTypeCache.Read(Request["Area"]);
+                configurationTemplateGuid = groupType.Guid;
+                RestParms = string.Format("?GroupTypeId={0}", Request["Area"]);
+            }
+            else if (Request["GroupTypeId"] != null)
+            {
+                int gtype = Int32.Parse(Request["GroupTypeId"]);
+                var groupType = Rock.Web.Cache.GroupTypeCache.Read(gtype);
+                configurationTemplateGuid = groupType.Guid;
+                RestParms = string.Format("?GroupTypeId={0}", gtype);
+            }
+            else if (Request["GroupId"] != null)
+            {
+                // get the root group type of this group
+                int groupId = Int32.Parse(Request["GroupId"]);
+
+                var rootGroupType = GetRootGroupType(groupId);
+                if (rootGroupType != null)
+                {
+                    configurationTemplateGuid = rootGroupType.Guid;
+                }
+                RestParms = string.Format("?GroupTypeId={0}", groupId);
+            }
+            else
+            {
+                if (!string.IsNullOrWhiteSpace(GetAttributeValue("GroupTypeTemplate")))
+                {
+                    configurationTemplateGuid = new Guid(GetAttributeValue("GroupTypeTemplate"));
+                }
+            }
+
+            if (configurationTemplateGuid == Guid.Empty)
+            {
+                lWarnings.Text = "<div class='alert alert-warning'>No check-in configuration template configured.</div>";
+            }
+            else
+            {
+                BuildHeirarchy(configurationTemplateGuid);
+                lContent.Text = _content.ToString();
+            }
+        }
+
+        private void BuildHeirarchy(Guid parentGroupTypeGuid)
+        {
+            GroupTypeService groupTypeService = new GroupTypeService(_rockContext);
+
+            var groupTypes = groupTypeService.Queryable("Groups, ChildGroupTypes").AsNoTracking()
+                            .Where(t => t.ParentGroupTypes.Select(p => p.Guid).Contains(parentGroupTypeGuid) && t.Guid != parentGroupTypeGuid).ToList();
+
+            foreach (var groupType in groupTypes)
+            {
+                if (groupType.GroupTypePurposeValueId == null || groupType.Groups.Count > 0)
+                {
+                    _content.Append("<ul>");
+                    _content.Append(string.Format("<li><strong>{0}</strong></li>", groupType.Name));
+                    if (groupType.ChildGroupTypes.Count > 0)
+                    {
+                        BuildHeirarchy(groupType.Guid);
+                    }
+
+                    _content.Append("<ul>");
+                    foreach (var group in groupType.Groups)
+                    {
+                        if (!string.IsNullOrWhiteSpace(GetAttributeValue("GroupDetailPage")))
+                        {
+                            var groupPageParams = new Dictionary<string, string>();
+                            if (Request["GroupTypeId"] != null)
+                            {
+                                groupPageParams.Add("GroupTypeId", Request["GroupTypeId"]);
+                            }
+                            groupPageParams.Add("GroupId", group.Id.ToString());
+                            _content.Append(string.Format("<li><a href='{0}'>{1}</a></li>", LinkedPageUrl("GroupDetailPage", groupPageParams), group.Name));
+                        }
+                        else
+                        {
+                            _content.Append(string.Format("<li>{0}</li>", group.Name));
+                        }
+                    }
+                    _content.Append("</ul>");
+
+                    _content.Append("</ul>");
+                }
+                else
+                {
+                    BuildHeirarchy(groupType.Guid);
+                }
+
+            }
+
+
+        }
+
+        #endregion
+    }
+}
---
+++ RockWeb/Plugins/org_newpointe/Checkin/CheckinGroupTreeView.ascx
@@ -0,0 +1,200 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="CheckinGroupTreeView.ascx.cs" Inherits="RockWeb.Plugins.org_newpointe.Checkin.CheckinGroupTreeView" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <asp:Panel ID="pnlView" runat="server" CssClass="panel panel-block">
+
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-check-square-o"></i>Check-in Areas</h1>
+            </div>
+            <div class="panel-body">
+                <asp:Literal ID="lWarnings" runat="server" />
+                <asp:Literal ID="lContent" runat="server" />
+
+
+                <asp:HiddenField ID="hfTreeViewData" runat="server" ClientIDMode="Static" />
+                <asp:HiddenField ID="hfTreeViewRestUrl" runat="server" ClientIDMode="Static" />
+                <asp:HiddenField ID="hfTreeViewRestParams" runat="server" ClientIDMode="Static" />
+                <asp:HiddenField ID="hfTreeViewSelectedId" runat="server" ClientIDMode="Static" />
+                <asp:HiddenField ID="hfTreeViewExpandedIds" runat="server" ClientIDMode="Static" />
+                <asp:HiddenField ID="hfPageRouteTemplate" runat="server" ClientIDMode="Static" />
+                <asp:HiddenField ID="hfSelectedIdQueryName" runat="server" ClientIDMode="Static" />
+                <asp:HiddenField ID="hfExpandedIdsQueryName" runat="server" ClientIDMode="Static" />
+
+                <div class="treeview">
+                    <div class="treeview-actions" id="divTreeviewActions" runat="server">
+
+                        <%--<div class="btn-group">
+                    <button type="button" class="btn btn-action btn-xs dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-plus-circle"></i> Add Location <span class="fa fa-caret-down"></span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><asp:LinkButton ID="lbAddLocationRoot" OnClick="lbAddLocationRoot_Click" Text="Add Top-Level" runat="server"></asp:LinkButton></li>
+                        <li><asp:LinkButton ID="lbAddLocationChild" OnClick="lbAddLocationChild_Click" Enabled="false" Text="Add Child To Selected" runat="server"></asp:LinkButton></li>
+                    </ul>
+                </div>--%>
+                    </div>
+
+                    <div class="treeview-scroll scroll-container scroll-container-horizontal">
+
+                        <div class="viewport">
+                            <div class="overview">
+                                <div class="panel-body treeview-frame">
+                                    <div id="treeview-content">
+                                        <%--<ul class="rocktree" id="TreeviewContentList" runat="server"></ul>--%>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="scrollbar">
+                            <div class="track">
+                                <div class="thumb">
+                                    <div class="end"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <script type="text/javascript">
+                    function deparam(querystring) {
+                        // remove any preceding url and split
+                        querystring = querystring.substring(querystring.indexOf('?') + 1).split('&');
+                        var params = {}, pair, d = decodeURIComponent;
+                        // march and parse
+                        for (var i = querystring.length - 1; i >= 0; i--) {
+                            pair = querystring[i].split('=');
+                            params[d(pair[0])] = d(pair[1]);
+                        }
+
+                        return params;
+                    };
+
+                    $(function () {
+
+                        var $data = $('#hfTreeViewData'),
+                            $restUrl = $('#hfTreeViewRestUrl'),
+                            $restParams = $('#hfTreeViewRestParams'),
+                            $selectedId = $('#hfTreeViewSelectedId'),
+                            $expandedIds = $('#hfTreeViewExpandedIds');
+
+
+                        var scrollbCategory = $('.treeview-scroll');
+                        scrollbCategory.tinyscrollbar({ axis: 'x', sizethumb: 60, size: 200 });
+
+                        // resize scrollbar when the window resizes
+                        $(document).ready(function () {
+                            $(window).on('resize', function () {
+                                resizeScrollbar(scrollbCategory);
+                            });
+                        });
+
+                        $('#treeview-content')
+                            .on('rockTree:selected', function (e, id) {
+
+                                var query = deparam(location.search);
+                                var selParameter = $('#hfSelectedIdQueryName').val();
+                                var expParameter = $('#hfExpandedIdsQueryName').val();
+
+
+                                query[selParameter] = id;
+
+
+                                var oldItemId = $selectedId.val();
+                                if (oldItemId !== id) {
+
+                                    // get the data-id values of rock-tree items that are showing visible children (in other words, Expanded Nodes)
+                                    var expandedDataIds = $(e.currentTarget).find('.rocktree-children').filter(":visible").closest('.rocktree-item').map(function () {
+                                        return $(this).attr('data-id')
+                                    }).get().join(',');
+
+
+                                    query[expParameter] = encodeURIComponent(expandedDataIds);
+
+
+                                    var pageRouteTemplate = $('#hfPageRouteTemplate').val();
+                                    var locationUrl = "";
+                                    var regex = new RegExp("{" + selParameter + "}", "i");
+
+                                    if (pageRouteTemplate.match(regex)) {
+                                        locationUrl = Rock.settings.get('baseUrl') + pageRouteTemplate.replace(regex, id);
+                                    }
+                                    else {
+                                        locationUrl = window.location.href.split('?')[0]
+                                    }
+
+                                    window.location = locationUrl + "?" + $.param(query);
+                                }
+                            })
+                            .on('rockTree:rendered', function () {
+
+                                // update viewport height
+                                resizeScrollbar(scrollbCategory);
+
+                            })
+                            .rockTree({
+                                restUrl: $restUrl.val() ? $restUrl.val() : null,
+                                restParams: $restParams.val() ? $restParams.val() : null,
+                                local: $data.val() ? JSON.parse($data.val()) : null,
+                                multiSelect: false,
+                                selectedIds: $selectedId.val() ? $selectedId.val().split(',') : null,
+                                expandedIds: $expandedIds.val() ? $expandedIds.val().split(',') : null
+                            });
+
+                        // Generic recursive utility function to find a node in the tree by its id
+                        _findNodeById = function (id, array) {
+                            var currentNode,
+                                node;
+
+                            if (!array || typeof array.length !== 'number') {
+                                return null;
+                            }
+
+                            for (var i = 0; i < array.length; i++) {
+                                currentNode = array[i];
+
+                                // remove surrounding single quotes from id if they exist
+                                var idCompare = id.toString().replace(/(^')|('$)/g, '');
+
+                                if (currentNode.id.toString() === idCompare) {
+                                    return currentNode;
+                                } else if (currentNode.hasChildren) {
+                                    node = _findNodeById(id, currentNode.children || []);
+
+                                    if (node) {
+                                        return node;
+                                    }
+                                }
+                            }
+
+                            return null;
+                        };
+
+
+                        var rt = $('#treeview-content').data("rockTree");
+                        var ev = $expandedIds.val().split(',');
+                        for (i = 0; i < ev.length; ++i) {
+                            node = _findNodeById(ev[i], rt.nodes);
+                            if (node !== null) {
+                                node.isOpen = true;
+                            }
+                        }
+                        rt.render();
+                    });
+
+                    function resizeScrollbar(scrollControl) {
+                        var overviewHeight = $(scrollControl).find('.overview').height();
+
+                        $(scrollControl).find('.viewport').height(overviewHeight);
+
+                        scrollControl.tinyscrollbar_update('relative');
+                    }
+                </script>
+            </div>
+
+        </asp:Panel>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
