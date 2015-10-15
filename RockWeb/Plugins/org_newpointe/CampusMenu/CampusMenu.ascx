036d488bd335bb797d4e8dfe8498eb419d01ff93
Bronson Witting <bwitting@newpointe.org>
2015-10-15 08:29:55 -0400

    Campus Menu Updates

--- RockWeb/Plugins/org_newpointe/CampusMenu/CampusMenu.ascx
+++ RockWeb/Plugins/org_newpointe/CampusMenu/CampusMenu.ascx
@@ -1,37 +1,5 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="CampusMenu.ascx.cs" Inherits="Plugins_org_newpointe_CampusMenu_CampusMenu" %>
 
-<div class="hidden-xs hidden-sm hidden-md">
-    <asp:Repeater ID="rptCampuses" runat="server">
-        <HeaderTemplate>
-            <ul id="npCampusMenu" class="nav navbar-nav">
-                <li role="presentation" class="dropdown npCampusMenu">
-                    <a class="dropdown-toggle" aria-expanded="false" aria-haspopup="true" role="button" data-toggle="dropdown" href="#">CHOOSE A LOCATION &nbsp;&nbsp;<i class="caret"></i></a>
-                    <ul class="dropdown-menu" style="z-index: 2500;">
-        </HeaderTemplate>
-        <ItemTemplate>
-            <li>
-                <asp:HyperLink CssClass="dropdown2" runat="server" ID="lnk" OnDataBinding="lnk_DataBinding"></asp:HyperLink></li>
-        </ItemTemplate>
-        <FooterTemplate>
-            </ul></li></ul>
-        </FooterTemplate>
-    </asp:Repeater>
-</div>
-
-<div id="mobileCampusMenu" class="hidden-lg">
-    <asp:Repeater ID="rptMobileCampuses" runat="server">
-        <HeaderTemplate>
-            <ul><li>CHOOSE A LOCATION</li></ul>
-            <ul class="ulCampusList">
-        </HeaderTemplate>
-        <ItemTemplate>
-            <li><asp:HyperLink runat="server" ID="lnk2" OnDataBinding="lnk_DataBinding"></asp:HyperLink></li>
-        </ItemTemplate>
-        <FooterTemplate>
-            </ul>
-        </FooterTemplate>
-    </asp:Repeater>
-</div>
 
 <div id="divCampusTimes" class="hidden-xs hidden-sm hidden-md col-lg-12">
     <h5 class="pull-right">WATCH LIVE EVERY WEEK! SUNDAYS AT 9 & 11 A.M.</h5>
--- RockWeb/Plugins/org_newpointe/CampusMenu/CampusMenu.ascx.cs
+++ RockWeb/Plugins/org_newpointe/CampusMenu/CampusMenu.ascx.cs
@@ -18,100 +18,14 @@ using System.Data;
 
 
 [DisplayName("Campus Menu")]
-[Category("Newpointe")]
+[Category("NewPointe.org Web Blocks")]
 [Description("Main menu")]
 
 public partial class Plugins_org_newpointe_CampusMenu_CampusMenu : RockBlock
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if(!Page.IsPostBack)
-        {
-            BindGrid();
-        }
+
     }
-
-    private void BindGrid()
-    {
-        CampusService cs = new CampusService(new RockContext());
-
-        var qry = cs.Queryable().Where(c => !c.Name.Contains("Central Services") && !c.Name.Contains("Future Campus")).Select(a => new { Name = a.Name.Replace("Services","").Trim(), URL = a.Url }).OrderBy(n => n.Name).ToList();
-
-        rptCampuses.DataSource = qry;
-        rptCampuses.DataBind();
-
-        rptMobileCampuses.DataSource = qry;
-        rptMobileCampuses.DataBind();
-    }
-
-    protected void lnk_DataBinding(object sender, EventArgs e)
-    {
-        var lnk = (System.Web.UI.WebControls.HyperLink)sender;
-        var name = Eval("Name").ToString();
-        var url = "/event-calendar";
-
-        name = name.Replace(" ", "").Replace("Campus", "");
-
-        switch (name.ToLower())
-        {
-            case "akron":
-                url += "?campusID=67713";
-                break;
-            case "canton":
-                url += "?campusID=53103";
-                break;
-            case "coshocton":
-                url += "?campusID=62004";
-                break;
-            case "dover":
-                url += "?campusID=51773";
-                break;
-            case "millersburg":
-                url += "?campusID=51774";
-                break;
-            case "wooster":
-                url += "?campusID=67714";
-                break;
-            default:
-                break;
-        }
-
-        lnk.NavigateUrl = url;
-
-
-        lnk.Text = name;
-    }
-    protected void lnk2_DataBinding(object sender, EventArgs e)
-    {
-        var lnk = (System.Web.UI.WebControls.HyperLink)sender;
-        var name = Eval("Name").ToString();
-        var url = "/EventCalendar";
-
-        switch (name.ToLower())
-        {
-            case "akron":
-                url += "?campusID=67713";
-                break;
-            case "canton":
-                url += "?campusID=53103";
-                break;
-            case "coshocton":
-                url += "?campusID=62004";
-                break;
-            case "dover":
-                url += "?campusID=51773";
-                break;
-            case "millersburg":
-                url += "?campusID=51774";
-                break;
-            case "wooster":
-                url += "?campusID=67714";
-                break;
-            default:
-                break;
-        }
-
-        lnk.NavigateUrl = url;
-        lnk.Text = name;
-    }
+    
 }
