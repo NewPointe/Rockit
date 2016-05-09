59697c1a94632f86d2c478790d4905651a15520f
Bronson Witting <bwitting@newpointe.org>
2016-05-09 16:59:54 -0400

    Added block to get Person URL Encoded Key from a PersonPicker

---
+++ RockWeb/Plugins/org_newpointe/PersonGetURLEncodedKey/PersonGetURLEncodedKey.ascx
@@ -0,0 +1,16 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PersonGetURLEncodedKey.ascx.cs" Inherits="RockWeb.Plugins.org_newpointe.PersonGetURLEncodedKey.PersonGetURLEncodedKey" %>
+ 
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <Rock:PersonPicker ID="ppPerson" runat="server" Label="Person" />
+            
+            <Rock:BootstrapButton ID="lbSave" runat="server" Text="Click Me" CssClass="btn btn-primary" OnClick="ppPerson_SelectPerson"
+    DataLoadingText="&lt;i class='fa fa-refresh fa-spin fa-2x'&gt;&lt;/i&gt; Saving" />
+        
+        
+<Rock:NotificationBox ID="nbSuccess" runat="server" Title="URL Encoded Key" Text="This is a success message." NotificationBoxType="Success" visible="false" />
+
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/org_newpointe/PersonGetURLEncodedKey/PersonGetURLEncodedKey.ascx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Attribute;
+
+
+namespace RockWeb.Plugins.org_newpointe.PersonGetURLEncodedKey
+{
+
+    /// <summary>
+    /// Block to pick a person and get their URL encoded key.
+    /// </summary>
+    [DisplayName("APerson Get URL Encoded Key")]
+    [Category("NewPointe Person")]
+    [Description("Gets the URL Enceded Key for the given person.")]
+    [BooleanField("Set to Logged In Person", "Yes", "No", "Choose whether or not you want to set the picker to the logged in person on page load.")]
+
+
+    public partial class PersonGetURLEncodedKey : Rock.Web.UI.RockBlock
+    {
+        //public Guid typeGuid;
+        RockContext rockContext = new RockContext();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Set the person picker to the currently logged in person.
+            PersonService personService = new PersonService(rockContext);
+            var personObject = personService.Get(CurrentPerson.Guid);
+            ppPerson.SetValue(personObject);
+        }
+
+        protected void ppPerson_SelectPerson(object sender, EventArgs e)
+        {
+            //Get the person from the PersonPicker
+            int? ppPersonAliasId = ppPerson.PersonAliasId;
+            int personAliasIdInt = ppPersonAliasId.Value;
+
+            PersonAliasService personAliasService = new PersonAliasService(rockContext);
+            var thePerson = personAliasService.Queryable().AsNoTracking()
+                .Where(a => a.AliasPersonId == personAliasIdInt)
+                .Select(a => a.Person)
+                .FirstOrDefault();
+
+            //Get the URL encoded key from the person object
+            var urlEncodedKey = thePerson.UrlEncodedKey;
+
+            //Show the key to the user
+            nbSuccess.Visible = true;
+            nbSuccess.Text = urlEncodedKey;
+            
+        }
+
+    }
+}
