68bc186b291f2f5353160e364fe9f7577ed8b826
Tyler Schrock <tschrock123@gmail.com>
2016-06-03 10:08:35 -0400

    Added template for new projects

---
+++ AddProject.ps1
@@ -0,0 +1,23 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$Name
+ )
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+Set-Location $scriptPath
+
+$prefix="org.newpointe"
+$templatename="$prefix.Template"
+$newprojectname="$prefix.$Name"
+$oldguid=[guid]::Empty.tostring()
+$newguid=[guid]::newguid().tostring()
+
+Copy-Item $templatename $newprojectname -Recurse
+
+Set-Location $newprojectname
+
+Move-Item "$templatename.csproj" -Destination "$newprojectname.csproj"
+
+(Get-Content "$newprojectname.csproj") -replace $templatename,$newprojectname -replace $oldguid,$newguid | Set-Content "$newprojectname.csproj"
+(Get-Content "Properties\AssemblyInfo.cs") -replace $templatename,$newprojectname -replace $oldguid,$newguid | Set-Content "Properties\AssemblyInfo.cs"
+
+Read-Host "Done"
