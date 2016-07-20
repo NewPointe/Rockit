3cd743c490b2f17b8a0ea05274322f25fac840cf
Bronson Witting <bwitting@newpointe.org>
2016-07-20 15:40:23 -0400

    Added Internal Shape Results block for person profile.

---
+++ RockWeb/Plugins/org_newpointe/Shape/ShapeResultsInternal.ascx
@@ -0,0 +1,222 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="ShapeResultsInternal.ascx.cs" Inherits="RockWeb.Plugins.org_newpointe.Shape.ShapeResultsInternal" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <Rock:NotificationBox runat="server" NotificationBoxType="Danger" Text="You must be logged in or access this page from a vaild link." Visible="False" ID="nbNoPerson" Title="No Valid Person"></Rock:NotificationBox>
+        
+ 
+        <h1 class="text-center">SHAPE Profile</h1>
+        <h2 class="text-center" style="margin-top: -10px;">
+            <asp:Label runat="server" ID="lbPersonName"></asp:Label></h2>
+
+        
+    <div class="panel panel-block"> 
+    <div class="panel-heading">
+        <h4 class="panel-title"><i class="fa fa-share-square-o"></i> Spiritual Gifts</h4>
+    </div>
+        <div class="panel panel-body">
+            <div class="col-md-6">
+                <h5 class="text-center">Spiritual Gift 1: </h5>
+                <h3 class="text-center" style="margin-top: -10px;">
+                    <asp:Label runat="server" ID="lbGift1Title"></asp:Label></h3>
+                <asp:Label runat="server" ID="lbGift1BodyHTML"></asp:Label>
+            </div>
+
+            <div class="col-md-6">
+                <h5 class="text-center">Spiritual Gift 2: </h5>
+                <h3 class="text-center" style="margin-top: -10px;">
+                    <asp:Label runat="server" ID="lbGift2Title"></asp:Label></h3>
+                <asp:Label runat="server" ID="lbGift2BodyHTML"></asp:Label>
+            </div>
+
+            <div class="col-md-12">
+                <br />
+            </div>
+        </div>
+        </div>
+
+
+    <div class="panel panel-block"> 
+    <div class="panel-heading">
+        <h4 class="panel-title"><i class="fa fa-share-square-o"></i> Heart and Abilities</h4>
+    </div>
+        <div class="panel panel-body">
+            <div class="col-md-6">
+                <h5 class="text-center">Heart/Ability 1: </h5>
+                <h3 class="text-center" style="margin-top: -10px;">
+                    <asp:Label runat="server" ID="lbHeart1Title"></asp:Label></h3>
+                <asp:Label runat="server" ID="lbHeart1BodyHTML"></asp:Label>
+            </div>
+
+            <div class="col-md-6">
+                <h5 class="text-center">Heart/Ability 2: </h5>
+                <h3 class="text-center" style="margin-top: -10px;">
+                    <asp:Label runat="server" ID="lbHeart2Title"></asp:Label></h3>
+                <asp:Label runat="server" ID="lbHeart2BodyHTML"></asp:Label>
+            </div>
+
+
+            <div class="col-md-12">
+                <br />
+            </div>
+
+        </div>
+        </div>
+
+
+
+    <div class="panel panel-block"> 
+    <div class="panel-heading">
+        <h4 class="panel-title"><i class="fa fa-share-square-o"></i> Personality (DISC Assessment)</h4>
+    </div>
+        <div class="panel panel-body">
+            
+            
+            <asp:Panel runat="server" ID="DISCResults" Visible="False">
+
+            <div class="col-md-12" >
+                
+                                    <ul class="discchart">
+                        <li class="discchart-midpoint"></li>
+                        <li style="height: 100%; width:0px;"></li>
+                        <li id="discNaturalScore_D" runat="server" class="discbar discbar-d">
+                            <div class="discbar-label">D</div>
+                        </li>
+                        <li id="discNaturalScore_I" runat="server" class="discbar discbar-i">
+                            <div class="discbar-label">I</div>
+                        </li>
+                        <li id="discNaturalScore_S" runat="server" class="discbar discbar-s">
+                            <div class="discbar-label">S</div>
+                        </li>
+                        <li id="discNaturalScore_C" runat="server" class="discbar discbar-c">
+                            <div class="discbar-label">C</div>
+                        </li>
+                    </ul>
+                
+
+                
+                <h3>Description</h3>
+                    <asp:Literal ID="lDescription" runat="server"></asp:Literal>
+
+                    <h3>Strengths</h3>
+                    <asp:Literal ID="lStrengths" runat="server"></asp:Literal>
+
+                    <h3>Challenges</h3>
+                    <asp:Literal ID="lChallenges" runat="server"></asp:Literal>
+
+                    <h3>Under Pressure</h3>
+                    <asp:Literal ID="lUnderPressure" runat="server"></asp:Literal>
+
+                    <h3>Motivation</h3>
+                    <asp:Literal ID="lMotivation" runat="server"></asp:Literal>
+
+                    <h3>Team Contribution</h3>
+                    <asp:Literal ID="lTeamContribution" runat="server"></asp:Literal>
+
+                    <h3>Leadership Style</h3>
+                    <asp:Literal ID="lLeadershipStyle" runat="server"></asp:Literal>
+
+                    <h3>Follower Style</h3>
+                    <asp:Literal ID="lFollowerStyle" runat="server"></asp:Literal>
+                
+                
+                <br />
+            </div>
+                
+                </asp:Panel>
+            
+            <asp:Panel runat="server" ID="NoDISCResults" Visible="True">
+
+            <div class="col-md-12 text-center" >
+                
+                <p>The DISC Assessment is used for the Personality portion of your SHAPE Profile.
+                    It looks like you haven't taken the DISC Assessment yet.</p>
+                <a href="https://newpointe.org/DISC?rckipid=<%= SelectedPerson.UrlEncodedKey %>" target="_blank" class="btn btn-primary np-button">TAKE THE DISC ASSESSMENT NOW</a>
+                
+                
+                <br />
+            </div>
+                
+                </asp:Panel>
+
+
+        </div>
+        </div>
+
+
+
+            <div class="panel panel-block"> 
+    <div class="panel-heading">
+        <h4 class="panel-title"><i class="fa fa-share-square-o"></i> Personality</h4>
+    </div>
+        <div class="panel panel-body">
+            <div class="col-md-12">
+                <h4 class="text-center">People </h4>
+                <p><asp:Label runat="server" ID="lbPeople"></asp:Label><br /></p>
+                
+                <h4 class="text-center">Places </h4>
+                <p><asp:Label runat="server" ID="lbPlaces"></asp:Label><br /></p>
+                
+                <h4 class="text-center">Events </h4>
+                <p><asp:Label runat="server" ID="lbEvents"></asp:Label><br /></p>
+            </div>
+
+            <div class="col-md-12">
+                <br />
+            </div>
+
+        </div>
+        </div>
+
+
+
+
+            <div class="panel panel-block"> 
+    <div class="panel-heading">
+        <h4 class="panel-title"><i class="fa fa-share-square-o"></i> Volunteer Opportunities</h4>
+    </div>
+        <div class="panel panel-body">
+
+            <div class="col-md-12">
+                <h2 class="text-center">Volunteer Opportunities</h2>
+                <p>Based on their SHAPE Profile, here are the volunteer opportunities that we recommended to <%= SelectedPerson.NickName %>:</p>
+
+                <asp:Repeater runat="server" ID="rpVolunteerOpportunities">
+                    <ItemTemplate>
+
+                        <div class="panel panel-default margin-t-md">
+                            <div class="panel-heading clearfix">
+                                <h1 class="panel-title pull-left">
+                                    <i class='<%# Eval("IconCssClass") %>'></i> <%# Eval("Name") %>
+                                </h1>
+                            </div>
+                            <div class="panel-body">
+                                <div class="col-md-12">
+                                    <p><%# Eval("Summary") %></p>
+                                    <a class="btn btn-default" href="https://newpointe.org/VolunteerOpportunities/<%# Eval("Id") %>" role="button">More Info</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </ItemTemplate>
+                </asp:Repeater>
+
+            </div>
+
+
+            <div class="col-md-12">
+                <br />
+            </div>
+            
+            
+
+        </div>
+                </div>
+        
+        <div class="col-md-12">
+                <br />
+            </div>
+
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/org_newpointe/Shape/ShapeResultsInternal.ascx.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Attribute;
+using Rock.Workflow;
+
+
+using System.Text.RegularExpressions;
+using System.Web.UI.HtmlControls;
+using Rock.Security;
+using Rock.Web.UI;
+
+namespace RockWeb.Plugins.org_newpointe.Shape
+{
+    /// <summary>
+    /// Template block for a TreeView.
+    /// </summary>
+    [DisplayName("SHAPE Results Internal")]
+    [Category("NewPointe Core")]
+    [Description("Shows a person the results of their SHAPE Assessment and recommends volunteer positions based on them.  Formatted for use on the internal Rock site.")]
+
+    public partial class ShapeResultsInternal : Rock.Web.UI.RockBlock
+    {
+        private RockContext rockContext = new RockContext();
+        public Person SelectedPerson;
+
+        public int SpiritualGift1;
+        public int SpiritualGift2;
+        public int Heart1;
+        public int Heart2;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+                if (!string.IsNullOrWhiteSpace(PageParameter("PersonId")))
+                {
+                    //Load the person based on the PersonId
+                    SelectedPerson = GetPersonFromId(PageParameter("PersonId"));
+                }
+
+                else
+                {
+                    //Show Error Message
+                    nbNoPerson.Visible = true;
+                    return;
+                }
+            
+
+            // Load the attributes
+
+            AttributeValueService attributeValueService = new AttributeValueService(rockContext);
+
+            var spiritualGift1 =
+                attributeValueService
+                    .Queryable()
+                    .FirstOrDefault(a => a.Attribute.Key == "SpiritualGift1" && a.EntityId == SelectedPerson.Id);
+
+            var spiritualGift2 =
+                attributeValueService
+                    .Queryable()
+                    .FirstOrDefault(a => a.Attribute.Key == "SpiritualGift2" && a.EntityId == SelectedPerson.Id);
+
+            var heart1 =
+                attributeValueService
+                    .Queryable().FirstOrDefault(a => a.Attribute.Key == "Heart1" && a.EntityId == SelectedPerson.Id);
+
+            var heart2 =
+                attributeValueService
+                    .Queryable().FirstOrDefault(a => a.Attribute.Key == "Heart2" && a.EntityId == SelectedPerson.Id);
+
+            var people = attributeValueService
+                .Queryable()
+                .FirstOrDefault(a => a.Attribute.Key == "SHAPEPeople" && a.EntityId == SelectedPerson.Id)
+                .Value;
+
+            var places = attributeValueService
+                .Queryable()
+                .FirstOrDefault(a => a.Attribute.Key == "SHAPEPlaces" && a.EntityId == SelectedPerson.Id)
+                .Value;
+
+            var events = attributeValueService
+                .Queryable()
+                .FirstOrDefault(a => a.Attribute.Key == "SHAPEEvents" && a.EntityId == SelectedPerson.Id)
+                .Value;
+
+
+            if (spiritualGift1 != null) SpiritualGift1 = Int32.Parse(spiritualGift1.Value);
+            if (spiritualGift2 != null) SpiritualGift2 = Int32.Parse(spiritualGift2.Value);
+            if (heart1 != null) Heart1 = Int32.Parse(heart1.Value);
+            if (heart2 != null) Heart2 = Int32.Parse(heart2.Value);
+
+
+
+            // Get all of the data about the assiciated gifts and heart categories
+
+            DefinedValueService definedValueService = new DefinedValueService(rockContext);
+
+            var shapeGift1Object = definedValueService.GetListByIds(new List<int> {SpiritualGift1}).FirstOrDefault();
+            var shapeGift2Object = definedValueService.GetListByIds(new List<int> {SpiritualGift2}).FirstOrDefault();
+            var heart1Object = definedValueService.GetListByIds(new List<int> {Heart1}).FirstOrDefault();
+            var heart2Object = definedValueService.GetListByIds(new List<int> {Heart2}).FirstOrDefault();
+
+            shapeGift1Object.LoadAttributes();
+            shapeGift2Object.LoadAttributes();
+            heart1Object.LoadAttributes();
+            heart2Object.LoadAttributes();
+
+
+
+            // Get Volunteer Opportunities
+
+            string gift1AssociatedVolunteerOpportunities =
+                shapeGift1Object.GetAttributeValue("AssociatedVolunteerOpportunities");
+            string gift2AssociatedVolunteerOpportunities =
+                shapeGift2Object.GetAttributeValue("AssociatedVolunteerOpportunities");
+            string allAssociatedVolunteerOpportunities = gift1AssociatedVolunteerOpportunities + "," +
+                                                         gift2AssociatedVolunteerOpportunities;
+
+            if (allAssociatedVolunteerOpportunities != ",")
+            {
+                List<int> associatedVolunteerOpportunitiesList =
+                    allAssociatedVolunteerOpportunities.Split(',').Select(t => int.Parse(t)).ToList();
+                Dictionary<int, int> VolunteerOpportunities = new Dictionary<int, int>();
+
+
+                var i = 0;
+                var q = from x in associatedVolunteerOpportunitiesList
+                    group x by x
+                    into g
+                    let count = g.Count()
+                    orderby count descending
+                    select new {Value = g.Key, Count = count};
+                foreach (var x in q)
+                {
+                    VolunteerOpportunities.Add(i, x.Value);
+                    i++;
+                }
+
+                int volunteerOpportunity1 = VolunteerOpportunities[0];
+                int volunteerOpportunity2 = VolunteerOpportunities[1];
+                int volunteerOpportunity3 = VolunteerOpportunities[2];
+                int volunteerOpportunity4 = VolunteerOpportunities[3];
+                ;
+
+                ConnectionOpportunityService connectionOpportunityService = new ConnectionOpportunityService(rockContext);
+
+                ConnectionOpportunity connectionOpportunityObject1 = new ConnectionOpportunity();
+                ConnectionOpportunity connectionOpportunityObject2 = new ConnectionOpportunity();
+                ConnectionOpportunity connectionOpportunityObject3 = new ConnectionOpportunity();
+                ConnectionOpportunity connectionOpportunityObject4 = new ConnectionOpportunity();
+
+                connectionOpportunityService.TryGet(volunteerOpportunity1, out connectionOpportunityObject1);
+                connectionOpportunityService.TryGet(volunteerOpportunity2, out connectionOpportunityObject2);
+                connectionOpportunityService.TryGet(volunteerOpportunity3, out connectionOpportunityObject3);
+                connectionOpportunityService.TryGet(volunteerOpportunity4, out connectionOpportunityObject4);
+
+                connectionOpportunityObject1.LoadAttributes();
+                connectionOpportunityObject2.LoadAttributes();
+                connectionOpportunityObject3.LoadAttributes();
+                connectionOpportunityObject4.LoadAttributes();
+
+                var connectionOpportunity1Roles = connectionOpportunityObject1.GetAttributeValue("Role");
+                var connectionOpportunity2Roles = connectionOpportunityObject2.GetAttributeValue("Role");
+                var connectionOpportunity3Roles = connectionOpportunityObject3.GetAttributeValue("Role");
+                var connectionOpportunity4Roles = connectionOpportunityObject4.GetAttributeValue("Role");
+
+                string connectionOpportunityRoles = connectionOpportunity1Roles + "," + connectionOpportunity2Roles +
+                                                    "," + connectionOpportunity3Roles + "," +
+                                                    connectionOpportunity4Roles;
+
+
+
+                List<ConnectionOpportunity> connectionOpportunityList = new List<ConnectionOpportunity>();
+
+                connectionOpportunityList.Add(connectionOpportunityObject1);
+                connectionOpportunityList.Add(connectionOpportunityObject2);
+                connectionOpportunityList.Add(connectionOpportunityObject3);
+                connectionOpportunityList.Add(connectionOpportunityObject4);
+
+                rpVolunteerOpportunities.DataSource = connectionOpportunityList;
+                rpVolunteerOpportunities.DataBind();
+            }
+
+
+
+
+
+
+            //Get DISC Info
+
+            DiscService.AssessmentResults savedScores = DiscService.LoadSavedAssessmentResults(SelectedPerson);
+
+            if (!savedScores.Equals(null))
+            {
+                ShowResults(savedScores);
+                DISCResults.Visible = true;
+                NoDISCResults.Visible = false;
+            }
+
+
+
+
+
+
+            // Build the UI
+
+            lbPersonName.Text = SelectedPerson.FullName;
+
+            lbGift1Title.Text = shapeGift1Object.Value;
+            lbGift1BodyHTML.Text = shapeGift1Object.GetAttributeValue("HTMLDescription");
+
+            lbGift2Title.Text = shapeGift2Object.Value;
+            lbGift2BodyHTML.Text = shapeGift2Object.GetAttributeValue("HTMLDescription");
+
+            lbHeart1Title.Text = heart1Object.Value;
+            lbHeart1BodyHTML.Text = heart1Object.GetAttributeValue("HTMLDescription");
+
+            lbHeart2Title.Text = heart2Object.Value;
+            lbHeart2BodyHTML.Text = heart2Object.GetAttributeValue("HTMLDescription");
+
+            lbPeople.Text = people;
+            lbPlaces.Text = places;
+            lbEvents.Text = events;
+
+
+        }
+
+
+
+
+        protected Person GetPersonFromForm(string formId)
+        {
+            AttributeValueService attributeValueService = new AttributeValueService(rockContext);
+            PersonService personService = new PersonService(rockContext);
+            PersonAliasService personAliasService = new PersonAliasService(rockContext);
+
+            var formAttribute = attributeValueService.Queryable().FirstOrDefault(a => a.Value == formId);
+            var person = personService.Queryable().FirstOrDefault(p => p.Id == formAttribute.EntityId);
+
+            return person;
+        }
+
+
+        protected Person GetPersonFromId(string PersonId)
+        {
+
+            int intPersonId = Int32.Parse(PersonId);
+            PersonService personService = new PersonService(rockContext);
+
+            var person = personService.Queryable().FirstOrDefault(p => p.Id == intPersonId);
+
+            return person;
+        }
+
+
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+
+
+
+        /// <summary>
+        /// Shows the results of the assessment test.
+        /// </summary>
+        /// <param name="savedScores">The saved scores.</param>
+        private void ShowResults(DiscService.AssessmentResults savedScores)
+        {
+            // Plot the Natural graph
+            DiscService.PlotOneGraph(discNaturalScore_D, discNaturalScore_I, discNaturalScore_S, discNaturalScore_C,
+                savedScores.NaturalBehaviorD, savedScores.NaturalBehaviorI, savedScores.NaturalBehaviorS,
+                savedScores.NaturalBehaviorC, 35);
+            ShowExplaination(savedScores.PersonalityType);
+
+        }
+
+        /// <summary>
+        /// Shows the explaination for the given personality type as defined in one of the
+        /// DefinedValues of the DISC Results DefinedType.
+        /// </summary>
+        /// <param name="personalityType">The one or two letter personality type.</param>
+        private void ShowExplaination(string personalityType)
+        {
+            var personalityValue =
+                DefinedTypeCache.Read(Rock.SystemGuid.DefinedType.DISC_RESULTS_TYPE.AsGuid())
+                    .DefinedValues.Where(v => v.Value == personalityType)
+                    .FirstOrDefault();
+            if (personalityValue != null)
+            {
+                lDescription.Text = personalityValue.Description;
+                lStrengths.Text = personalityValue.GetAttributeValue("Strengths");
+                lChallenges.Text = personalityValue.GetAttributeValue("Challenges");
+                lUnderPressure.Text = personalityValue.GetAttributeValue("UnderPressure");
+                lMotivation.Text = personalityValue.GetAttributeValue("Motivation");
+                lTeamContribution.Text = personalityValue.GetAttributeValue("TeamContribution");
+                lLeadershipStyle.Text = personalityValue.GetAttributeValue("LeadershipStyle");
+                lFollowerStyle.Text = personalityValue.GetAttributeValue("FollowerStyle");
+            }
+        }
+
+
+    }
+}
