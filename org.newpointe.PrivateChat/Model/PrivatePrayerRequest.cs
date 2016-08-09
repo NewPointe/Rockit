479a803513bbe1b9c1cd424eb8eed653d2985af6
Aaron L Jones <jones@rmrdevelopment.com>
2016-08-09 11:29:48 -0400

    Newpointe Institute and Live
    Included is the themes for the institute.newpointe site and
    live.newpointe site.
    
    Live.newpointe includes integration with Firebase and custom plugins for
    private prayer.
    
    Institute.newpointe consists of the theme with some custom lava
    templates for displaying tables from content channels.

---
+++ org.newpointe.PrivateChat/Model/PrivatePrayerRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Runtime.Serialization;
+
+using Rock.Data;
+using Rock.Model;
+
+using org.newpointe.PrivateChat.Data;
+
+namespace org.newpointe.PrivateChat.Model
+{
+    [Table( "_org_newpointe_PrivateChat_PrivatePrayerRequest")]
+    [DataContract]
+    public class PrivatePrayerRequest : Rock.Data.Model<PrivatePrayerRequest>, Rock.Security.ISecured
+    {
+        
+        //public virtual Person Person { get; set; }
+
+        [DataMember]
+        public int? Person_Id { get; set; }
+        
+        [Required( ErrorMessage = "RoomId is required")]
+        [DataMember]
+        public string RoomId { get; set; }
+
+        [Required]
+        [DataMember]
+        public bool Answered { get; set; }
+
+        [Required]
+        [DataMember]
+        public string Name { get; set; }
+    }
+
+    public partial class PrivatePrayerRequestConfiguration : EntityTypeConfiguration<PrivatePrayerRequest>
+    {
+        public PrivatePrayerRequestConfiguration()
+        {
+            //this.HasOptional(r => r.Person).WithMany().HasForeignKey(r => r.Person_Id).WillCascadeOnDelete(false);
+        }
+    }
+}
