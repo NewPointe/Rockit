ed83d2803d59d9b0b7fa0fcf2ebf8e03c89bb950
bwitting <bwitting@newpointe.org>
2016-11-09 16:02:06 -0500

    Initial Commit of Stars plugin

---
+++ org.newpointe.Stars/SystemGuid/DefinedType.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace org.newpointe.Stars.SystemGuid
+{
+    /// <summary>
+    /// System Defined Types
+    /// </summary>
+    public static class DefinedType
+    {
+        /// <summary>
+        /// Types of Referral Agencies
+        /// </summary>
+        public const string STARS_TYPE = "048C200E-2B8E-4501-8C9B-A87C870C3C0D";
+    }
+}
