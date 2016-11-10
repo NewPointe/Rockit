e17bdb81e646c39ec8dd72ad39f44da86ca4c655
Tyler Schrock <tschrock123@gmail.com>
2016-11-10 10:25:52 -0500

    Add simplified New Family block

---
+++ RockWeb/Plugins/org_newpointe/BetterNewFamily/NFCINewFamily.ascx
@@ -0,0 +1,105 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="NFCINewFamily.ascx.cs" Inherits="RockWeb.Plugins.org_newpointe.BetterNewFamily.NFCINewFamily" %>
+
+<asp:UpdatePanel ID="upAddGroup" runat="server">
+    <ContentTemplate>
+        
+        <div class="panel panel-block">
+            <div class="panel-heading">
+                <h1 class="panel-title">
+                    <i class="fa fa-plus-square-o"></i><asp:Literal ID="lTitle" runat="server"></asp:Literal>
+                </h1>
+            </div>
+            <div class="panel-body">
+
+                <asp:ValidationSummary ID="valSummaryTop" runat="server" HeaderText="Please Correct the Following" CssClass="alert alert-danger" />
+
+                <asp:Panel runat="server" ID="pnlNewFamily">
+                    <h2>Parent Info</h2>
+                    <div class="row">
+                        <div class="col-md-6"><Rock:RockTextBox runat="server" ID="rtbParentFirstName" Label="First Name" Required="true" RequiredErrorMessage="Parent's First Name is Required"></Rock:RockTextBox></div>
+                        <div class="col-md-6"><Rock:RockTextBox runat="server" ID="rtbParentLastName" Label="Last Name" Required="true" RequiredErrorMessage="Parent's Last Name is Required"></Rock:RockTextBox></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6"><Rock:PhoneNumberBox runat="server" ID="pnbParentPhoneNumber" Label="Parent Phone Number" Required="true" RequiredErrorMessage="Parent's Phone Number is Required"></Rock:PhoneNumberBox></div>
+                        <div class="col-md-6"><Rock:CampusPicker runat="server" ID="cpCampus" Label="Campus" Required="true" RequiredErrorMessage="Campus is Required"></Rock:CampusPicker></div>
+                    </div>
+    
+                    <h2>Children</h2>
+                    <table class="table table-groupmembers">
+                        <thead>
+                            <tr>
+                                <th class="required">First Name</th>
+                                <th class="required">Last Name</th>
+                                <th class="required">Gender</th>
+                                <th>Birthdate</th>
+                                <th class="required">Grade</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <asp:Repeater runat="server" ID="rKids" OnItemDataBound="rKids_ItemDataBound" OnItemCommand="rKids_ItemCommand">
+                                <ItemTemplate>
+                                    <tr>
+                                        <td><Rock:RockTextBox runat="server" ID="rtpKidFirstName" Required="true" RequiredErrorMessage="First Name is Required for all Children"></Rock:RockTextBox></td>
+                                        <td><Rock:RockTextBox runat="server" ID="rtpKidLastName" Required="true" RequiredErrorMessage="Last Name is Required for all Children"></Rock:RockTextBox></td>
+                                        <td>
+                                            <Rock:RockRadioButtonList runat="server" ID="rblGender" Required="true" RequiredErrorMessage="Gender is Required for all Children">
+                                                <asp:ListItem Text="Male" Value="1" />
+                                                <asp:ListItem Text="Female" Value="2" />
+                                            </Rock:RockRadioButtonList>
+                                        </td>
+                                        <td><Rock:DatePicker runat="server" ID="dpBirthdate"></Rock:DatePicker></td>
+                                        <td><Rock:GradePicker runat="server" ID="gpGrade" Required="true" Label="" RequiredErrorMessage="Grade is Required for all Children"></Rock:GradePicker></td>
+                                        <td><asp:LinkButton ID="lbDelete" runat="server" Text='<i class="fa fa-times"></i>' CssClass="btn btn-danger" CommandName="Delete" CausesValidation="false"></asp:LinkButton></td>
+                                    </tr>
+                                </ItemTemplate>
+                            </asp:Repeater>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="5"></td>
+                                <td><asp:LinkButton runat="server" ID="lbAddKid" Text='<i class="fa fa-plus"></i>' CssClass="btn btn-success" OnClick="lbAddKid_Click" CausesValidation="false" /></td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </asp:Panel>
+
+                <asp:Panel runat="server" ID="pnlExtra" Visible="false">
+                
+                <h2>Extra Info</h2>
+                    <table class="table table-groupmembers">
+                        <thead>
+                            <tr>
+                                <th>Child</th>
+                                <th>Ability Level</th>
+                                <th>Allergy</th>
+                                <th>Legal Notes</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <asp:Repeater runat="server" ID="rExtras" OnItemDataBound="rExtras_ItemDataBound">
+                                <ItemTemplate>
+                                    <tr>
+                                        <td><Rock:RockLiteral runat="server" ID="rlKidName"></Rock:RockLiteral></td>
+                                        <td><Rock:DefinedValuePicker runat="server" ID="dvpAbilityLevel"></Rock:DefinedValuePicker></td>
+                                        <td><Rock:RockTextBox runat="server" ID="rtbAllergy"></Rock:RockTextBox></td>
+                                        <td><Rock:RockTextBox runat="server" ID="rtbLegalNotes"></Rock:RockTextBox></td>
+                                    </tr>
+                                </ItemTemplate>
+                            </asp:Repeater>
+                        </tbody>
+                    </table>
+                
+                </asp:Panel>
+
+                <div class="actions">
+                    <asp:LinkButton runat="server" ID="lbBack" CssClass="btn btn-default" Text="Back" OnClick="lbBack_Click"></asp:LinkButton>
+                    <asp:LinkButton runat="server" ID="lbNext" CssClass="btn btn-primary" Text="Next" OnClick="lbNext_Click"></asp:LinkButton>
+                    <asp:LinkButton runat="server" ID="lbSubmit" CssClass="btn btn-primary" Text="Submit" OnClick="lbSubmit_Click"></asp:LinkButton>
+                </div>
+            
+            </div>
+        </div>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/org_newpointe/BetterNewFamily/NFCINewFamily.ascx.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.org_newpointe.BetterNewFamily
+{
+    /// <summary>
+    /// Block for adding new families
+    /// </summary>
+    [DisplayName( "NFCI New Family" )]
+    [Category( "NewPointe Check-in" )]
+    [Description( "Allows for quickly adding a new Family" )]
+
+    [LinkedPage( "Person Details Page", "The page to use to show person details.", false, "", "", 0 )]
+    public partial class NFCINewFamily : Rock.Web.UI.RockBlock
+    {
+        List<KidData> kidsList = new List<KidData>();
+        int _abilityLevelDefinedTypeId;
+
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+
+                ShowPage( 1 );
+
+                //kidsList.Add( new KidData { FirstName = "", LastName = "", Gender = Gender.Unknown } );
+                BindControls();
+
+                var contextCampus = RockPage.GetCurrentContext( EntityTypeCache.Read( "Rock.Model.Campus" ) ) as Campus;
+                if ( contextCampus != null )
+                {
+                    cpCampus.SelectedCampusId = contextCampus.Id;
+                }
+            }
+            else
+            {
+                loadKids();
+            }
+        }
+
+        protected void BindControls()
+        {
+            _abilityLevelDefinedTypeId = DefinedTypeCache.Read( Rock.SystemGuid.DefinedType.PERSON_ABILITY_LEVEL_TYPE.AsGuid() ).Id;
+
+            var camp = cpCampus.SelectedCampusId;
+            cpCampus.Campuses = CampusCache.All( false );
+            cpCampus.SelectedCampusId = camp;
+
+            rKids.DataSource = kidsList;
+            rKids.DataBind();
+            rExtras.DataSource = kidsList;
+            rExtras.DataBind();
+
+        }
+
+        private class KidData
+        {
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            public Gender Gender { get; set; }
+            public DateTime? Birthdate { get; set; }
+            public DefinedValueCache Grade { get; set; }
+            public DefinedValueCache AbilityLevel { get; set; }
+            public String Allergy { get; set; }
+            public String LegalNotes { get; set; }
+        }
+
+        protected void loadKids()
+        {
+            kidsList.Clear();
+            foreach ( RepeaterItem Item in rKids.Items )
+            {
+                RockTextBox firstName = Item.FindControl( "rtpKidFirstName" ) as RockTextBox;
+                RockTextBox lastName = Item.FindControl( "rtpKidLastName" ) as RockTextBox;
+                RockRadioButtonList gender = Item.FindControl( "rblGender" ) as RockRadioButtonList;
+                DatePicker birthdate = Item.FindControl( "dpBirthdate" ) as DatePicker;
+                GradePicker grade = Item.FindControl( "gpGrade" ) as GradePicker;
+
+                var kidData = new KidData
+                {
+                    FirstName = firstName.Text,
+                    LastName = lastName.Text,
+                    Gender = gender.SelectedValueAsEnum<Gender>( Gender.Unknown ),
+                    Birthdate = birthdate.SelectedDate,
+                    Grade = grade.SelectedGradeValue
+                };
+
+                if ( rExtras.Items.Count > Item.ItemIndex )
+                {
+                    RepeaterItem extraItem = rExtras.Items[Item.ItemIndex];
+                    DefinedValuePicker ability = extraItem.FindControl( "dvpAbilityLevel" ) as DefinedValuePicker;
+                    RockTextBox allergy = extraItem.FindControl( "rtbAllergy" ) as RockTextBox;
+                    RockTextBox legal = extraItem.FindControl( "rtbLegalNotes" ) as RockTextBox;
+
+                    kidData.AbilityLevel = DefinedValueCache.Read( ability.SelectedValueAsId() ?? -1 );
+                    kidData.Allergy = allergy.Text;
+                    kidData.LegalNotes = legal.Text;
+                }
+
+                kidsList.Add( kidData );
+            }
+        }
+
+        protected void rKids_ItemDataBound( object sender, RepeaterItemEventArgs e )
+        {
+            RepeaterItem Item = e.Item;
+            if ( Item.ItemType == ListItemType.Item || Item.ItemType == ListItemType.AlternatingItem )
+            {
+                var kid = e.Item.DataItem as KidData;
+
+                RockTextBox firstName = Item.FindControl( "rtpKidFirstName" ) as RockTextBox;
+                RockTextBox lastName = Item.FindControl( "rtpKidLastName" ) as RockTextBox;
+                RockRadioButtonList gender = Item.FindControl( "rblGender" ) as RockRadioButtonList;
+                DatePicker birthdate = Item.FindControl( "dpBirthdate" ) as DatePicker;
+                GradePicker grade = Item.FindControl( "gpGrade" ) as GradePicker;
+
+                firstName.Text = kid.FirstName;
+                lastName.Text = kid.LastName;
+                gender.SelectedValue = kid.Gender.ConvertToInt().ToString();
+                birthdate.SelectedDate = kid.Birthdate;
+                grade.SelectedGradeValue = kid.Grade;
+            }
+        }
+
+        protected void rExtras_ItemDataBound( object sender, RepeaterItemEventArgs e )
+        {
+            RepeaterItem Item = e.Item;
+            if ( Item.ItemType == ListItemType.Item || Item.ItemType == ListItemType.AlternatingItem )
+            {
+                var kid = e.Item.DataItem as KidData;
+
+                RockLiteral name = Item.FindControl( "rlKidName" ) as RockLiteral;
+                DefinedValuePicker ability = Item.FindControl( "dvpAbilityLevel" ) as DefinedValuePicker;
+                RockTextBox allergy = Item.FindControl( "rtbAllergy" ) as RockTextBox;
+                RockTextBox legal = Item.FindControl( "rtbLegalNotes" ) as RockTextBox;
+
+                name.Text = kid.FirstName + ' ' + kid.LastName;
+                ability.DefinedTypeId = _abilityLevelDefinedTypeId;
+                ability.SelectedValue = kid.AbilityLevel != null ? kid.AbilityLevel.Id.ToString() : "";
+                allergy.Text = kid.Allergy;
+                legal.Text = kid.LegalNotes;
+            }
+        }
+
+
+        protected void lbAddKid_Click( object sender, EventArgs e )
+        {
+            kidsList.Add( new KidData { FirstName = "", LastName = rtbParentLastName.Text, Gender = Gender.Unknown } );
+            BindControls();
+        }
+
+        protected void rKids_ItemCommand( object source, RepeaterCommandEventArgs e )
+        {
+            if ( e.CommandName == "Delete" )
+            {
+                kidsList.RemoveAt( e.Item.ItemIndex );
+                BindControls();
+            }
+        }
+
+        protected void ShowPage( int pageNumber )
+        {
+            pnlNewFamily.Visible = pageNumber == 1;
+            pnlExtra.Visible = pageNumber != 1;
+            lbBack.Visible = pageNumber != 1;
+            lbNext.Visible = pageNumber == 1;
+            lbSubmit.Visible = pageNumber != 1;
+        }
+
+        protected void lbBack_Click( object sender, EventArgs e )
+        {
+            ShowPage( 1 );
+        }
+
+        protected void lbNext_Click( object sender, EventArgs e )
+        {
+            if ( Page.IsValid )
+            {
+                ShowPage( 2 );
+                BindControls();
+            }
+        }
+
+        protected void lbSubmit_Click( object sender, EventArgs e )
+        {
+
+            if ( Page.IsValid )
+            {
+                RockContext rockContext = new RockContext();
+
+                var adultRole = new GroupTypeRoleService( rockContext ).Get( Rock.SystemGuid.GroupRole.GROUPROLE_FAMILY_MEMBER_ADULT.AsGuid() );
+                var childRole = new GroupTypeRoleService( rockContext ).Get( Rock.SystemGuid.GroupRole.GROUPROLE_FAMILY_MEMBER_CHILD.AsGuid() );
+
+                Person parent = new Person();
+                parent.FirstName = rtbParentFirstName.Text;
+                parent.LastName = rtbParentLastName.Text;
+
+                parent.LoadAttributes( rockContext );
+
+                PhoneNumber parentPhone = new PhoneNumber();
+                parentPhone.Number = pnbParentPhoneNumber.Number;
+
+                parent.PhoneNumbers.Add( parentPhone );
+
+                List<GroupMember> familyMembers = new List<GroupMember>();
+
+                GroupMember parentGM = new GroupMember();
+                parentGM.Person = parent;
+                parentGM.GroupRoleId = adultRole.Id;
+
+                familyMembers.Add( parentGM );
+
+                foreach ( KidData kidData in kidsList )
+                {
+                    Person kid = new Person();
+                    kid.FirstName = kidData.FirstName;
+                    kid.LastName = kidData.LastName;
+                    kid.Gender = kidData.Gender;
+                    kid.SetBirthDate( kidData.Birthdate );
+                    kid.GradeOffset = kidData.Grade.Value.AsIntegerOrNull();
+
+                    kid.LoadAttributes( rockContext );
+                    kid.SetAttributeValue( "AbilityLevel", kidData.AbilityLevel.Guid.ToString() );
+                    kid.SetAttributeValue( "Allergy", kidData.Allergy );
+                    kid.SetAttributeValue( "LegalNotes", kidData.LegalNotes );
+
+                    GroupMember kidGM = new GroupMember();
+                    kidGM.Person = kid;
+                    kidGM.GroupRoleId = childRole.Id;
+
+                    familyMembers.Add( kidGM );
+                }
+
+                Group family = GroupService.SaveNewFamily( rockContext, familyMembers, cpCampus.SelectedValueAsInt(), true );
+
+                if ( !String.IsNullOrWhiteSpace( GetAttributeValue( "PersonDetailsPage" ) ) )
+                {
+                    NavigateToLinkedPage( "PersonDetailsPage", new Dictionary<string, string>() { { "PersonId", parent.Id.ToString() } } );
+                }
+                else
+                {
+                    NavigateToCurrentPage( new Dictionary<string, string>() { { "PersonId", parent.Id.ToString() } } );
+                }
+
+            }
+
+        }
+
+    }
+}
