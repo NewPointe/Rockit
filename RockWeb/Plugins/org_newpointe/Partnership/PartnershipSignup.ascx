c12d53c8e4b710a569bad875ac7bc67d411e55d5
bwitting <bwitting@newpointe.org>
2016-12-09 15:38:51 -0500

    Add PartnershipSignup block

---
+++ RockWeb/Plugins/org_newpointe/Partnership/PartnershipSignup.ascx
@@ -0,0 +1,139 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PartnershipSignup.ascx.cs" Inherits="RockWeb.Plugins.org_newpointe.Partnership.PartnershipSignup" %>
+
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        
+        <Rock:ModalAlert ID="mdNotLoggedIn" runat="server" />
+
+ 
+        <asp:Panel ID="pnlSignup" runat="server" CssClass="panel panel-block" Visible="True">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-user"></i> NewPointe Partnership Agreement</h1>
+            </div>
+            <div class="panel-body">
+
+                <h2>Partnership Covenant for <asp:Literal runat="server" ID="lPersonInfo"></asp:Literal></h2>
+                
+                <p>Our Mission: To lead people realize and reach their full potential in Jesus Christ.</p>
+                
+                <p>As a partner with NewPointe Community Church, I am committing to uphold these values as we work together to fulfill our mission. And as we uphold these values, we will create a welcoming and beautiful community of Christ followers.</p>
+                
+                <ol>
+                   <li>I will protect the unity of my church
+                        <ul>
+                            <li>by acting in love toward others <small>(1 Peter 1:22)</small></li>
+                            <li>by refusing to gossip <small>(Ephesians 4:29)</small></li>
+                            <li>by submitting to church leadership <small>(Hebrews 13:17)</small></li>
+                        </ul>
+                    </li>
+                    
+                    <li>I will share the responsibilities of my church
+                        <ul>
+                            <li>by praying for our church <small>(1 Thessalonians 1:2)</small></li>
+                            <li>by inviting unconnected people to church <small>(Luke 14:23)</small></li>
+                            <li>by welcoming those who visit our church <small>(Romans 15:7)</small></li>
+                        </ul>
+                    </li>
+                    
+                    <li>I will serve my church
+                        <ul>
+                            <li>by cultivating a servant’s heart</li>
+                            <li>by using my spiritual gift in my church <small>(1 Peter 4:10)</small></li>
+                            <li>by participating in my church’s training gatherings <small>(Ephesians 4:11-12)</small></li>
+                        </ul>
+                    </li>
+                    
+                    <li>I will support my church
+                        <ul>
+                            <li>by attending faithfully <small>(Hebrews 10:25)</small></li>
+                            <li>by pursuing spiritual growth and holy living <small>(Philippians 1:27)</small></li>
+                            <li>by giving financially <small>(1 Corinthians 16:2 & Leviticus 27:30)</small></li>
+                        </ul>
+                    </li>
+                    
+                </ol>
+                
+                <br /><br />
+                <ul>
+                    <li>To develop these values within my personal life, I am committing to
+                        <ul>
+                            <li>faithfully pray for the ministry of NewPointe</li>
+                            <li>use my spiritual gifts in ministry at NewPointe</li>
+                            <li>give financially at NewPointe</li>
+                            <li>attend regularly at NewPointe</li>
+                        </ul>
+                    </li>
+                </ul>
+                
+                <br /><br />
+                
+            </div>
+        </asp:Panel>
+        
+        
+        
+        <asp:Panel ID="pnlSignature" runat="server" CssClass="panel panel-block" Visible="True">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-user"></i> NewPointe Partnership Agreement</h1>
+            </div>
+            <div class="panel-body">
+
+                <p>By signing <small>(typing my name in the box)</small> below, I agree to...</p>
+
+               <Rock:RockTextBox runat="server" ID="tbSignature" Label="Signature"/>
+                
+                <Rock:BootstrapButton runat="server" ID="btnSubmit" DataLoadingText="Submitting Partnership Covenant" CssClass="btn btn-newpointe" OnClick="btnSubmit_OnClick">Submit</Rock:BootstrapButton>
+                
+                
+                <br /><br />
+
+            </div>
+        </asp:Panel>
+
+        
+        
+        <asp:Panel ID="pnlOpportunities" runat="server" CssClass="panel panel-block" Visible="False">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-user"></i> NewPointe Partnership Covenant</h1>
+            </div>
+            <div class="panel-body">
+                
+                <h4>Thank you! Your Partnership Covenant has been submitted.</h4>
+
+                <div class="well">
+                <h3>My Partnership Opportunities</h3>
+                
+                <p><strong>Serving:</strong> <asp:Literal runat="server" ID="lServing"></asp:Literal> </p>
+                <p><strong>Giving:</strong> <asp:Literal runat="server" ID="lGiving"></asp:Literal> </p>
+                <p><strong>DISCOVER My Church:</strong> <asp:Literal runat="server" ID="lDiscover"></asp:Literal> </p> 
+                
+                </div>
+
+            </div>
+        </asp:Panel>
+        
+        
+        
+        
+        
+        
+        
+        <asp:Panel ID="pnlNotLoggedIn" runat="server" CssClass="panel panel-block" Visible="False">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-user"></i> NewPointe Partnership Agreement</h1>
+            </div>
+            <div class="panel-body">
+
+                <Rock:NotificationBox runat="server" NotificationBoxType="Danger" Text="You must be logged to sign the Partnership Covenant. " Visible="True" ID="nbNoPerson" Title="No Valid Person"></Rock:NotificationBox>
+
+            </div>
+        </asp:Panel>
+        
+        
+
+
+    </ContentTemplate>
+</asp:UpdatePanel>
+
+
---
+++ RockWeb/Plugins/org_newpointe/Partnership/PartnershipSignup.ascx.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Attribute;
+using Rock.Communication;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Web;
+using DotLiquid.Tags;
+
+namespace RockWeb.Plugins.org_newpointe.Partnership
+{
+
+    /// <summary>
+    /// Block to pick a person and get their URL encoded key.
+    /// </summary>
+    [DisplayName( "Partnership Signup" )]
+    [Category( "NewPointe Partnership" )]
+    [Description("Partnership Signup")]
+
+
+
+    public partial class PartnershipSignup : Rock.Web.UI.RockBlock
+    {
+
+        RockContext rockContext = new RockContext();
+        Person _activePerson = new Person();
+
+
+        protected override void OnLoad( EventArgs e )
+        {
+            if ( !IsPostBack )
+            {
+                
+            }
+
+            if (CurrentPerson != null)
+            {
+                _activePerson = CurrentPerson;
+                var currentPersonCampus = CurrentPerson.GetCampus();
+
+                string lastAttended = GetDiscoverAttendanceInfo(_activePerson);
+                string volunteerGroups = GetServingInfo(_activePerson);
+
+                lDiscover.Text = String.Format( "You attended DISCOVER My Church on {0}.", lastAttended);
+
+                if (lastAttended == "never")
+                {
+                    lDiscover.Text = "Upcoming DISCOVER My Church Opportunities at the " + currentPersonCampus.Name + ":<br />" + UpcomingDiscover(currentPersonCampus, "DISCOVER My Church");
+                }
+
+                lServing.Text = volunteerGroups;
+
+                lGiving.Text = "Thanks for Giving! Your donations are making an eternal difference.  <a href= \"https://newpointe.org/GiveNow \">Click here</a> to start or manage your online giving at NewPointe - it's quick and easy!";
+
+                lPersonInfo.Text = _activePerson.FullName;
+
+            }
+            else
+            {
+                string path = HttpContext.Current.Request.Url.AbsolutePath;
+                path = path.UrlEncode();
+
+                mdNotLoggedIn.Show( string.Format("Before you can sign the Partnership Covenant, you must log in with your MyNewPointe account.<br /><br /> <p class=\"text-center\"><a href = \"https://newpointe.org/Login?returnurl={0} \" class=\"btn btn-newpointe\">LOG IN</a> <a href = \"https://newpointe.org/NewAccount?returnurl={0} \" class=\"btn btn-newpointe\">REGISTER</a></p>", path), ModalAlertType.Alert);
+
+                pnlSignup.Visible = false;
+                pnlNotLoggedIn.Visible = true;
+                pnlSignature.Visible = false;
+            }
+
+
+        }
+
+        protected string GetDiscoverAttendanceInfo(Person thePerson)
+        {
+            string lastAttendedDate = "never";
+
+            AttendanceService attendanceService = new AttendanceService(rockContext);
+            GroupService groupService = new GroupService(rockContext);
+
+            var discoverGroups = groupService.Queryable().Where(g => g.Name.Contains("DISCOVER My Church"));
+
+            var discoverGroupAttendance = attendanceService.Queryable().Where(a => discoverGroups.Contains(a.Group) && a.PersonAliasId == thePerson.PrimaryAliasId);
+
+            if (discoverGroupAttendance.Any())
+            {
+                discoverGroupAttendance = discoverGroupAttendance.OrderByDescending(a => a.StartDateTime);
+                lastAttendedDate =
+                    discoverGroupAttendance.Select(a => a.StartDateTime).FirstOrDefault().ToShortDateString();
+            }
+
+            return lastAttendedDate;
+        }
+
+
+        protected string UpcomingDiscover(Campus theCampus, string theTitle)
+        {
+
+            string upcomingDiscovers = "";
+
+            ContentChannelItemService contentChannelItemService = new ContentChannelItemService(rockContext);
+
+            List<ContentChannelItem> contentChannelItemsList = new List<ContentChannelItem>();
+
+            var upcoming =
+                contentChannelItemService.Queryable().Where(a => a.ContentChannelId == 14 && a.Title.Contains(theTitle) && a.StartDateTime >= DateTime.Now);
+
+
+            foreach (var x in upcoming)
+            {
+                x.LoadAttributes();
+
+                var campus = x.AttributeValues["Campus"];
+
+                if (campus.ValueFormatted == theCampus.Name)
+                    {
+                    contentChannelItemsList.Add(x);
+                    }
+
+            }
+
+            foreach (var x in contentChannelItemsList)
+            {
+                x.LoadAttributes();
+
+                string registrationLink = "";
+
+                if (x.AttributeValues["RegistrationLink"].ValueFormatted != "")
+                {
+                    registrationLink = String.Format("<a href= \"{0}\">Register Now!",
+                        x.AttributeValues["RegistrationLink"].Value);
+                }
+
+                upcomingDiscovers += String.Format("Date: {0} at {1}. Location: {2}. {3} <br>", x.StartDateTime.ToShortDateString(),
+                    x.StartDateTime.ToShortTimeString(), x.AttributeValues["Location"], registrationLink);
+            }
+
+            if (!contentChannelItemsList.Any())
+            {
+                upcomingDiscovers = String.Format("There are not upcoming {0} Opportinuties at the {1}.", theTitle,
+                    theCampus.Name);
+            }
+     
+
+            return upcomingDiscovers;
+        }
+
+
+
+        protected string GetServingInfo(Person thePerson)
+        {
+
+            string volunteerGroupsString = "<a href= \"https://newpointe.org/VolunteerOpportunities \">Click here</a> to check out some incredible serving opportunities at NewPointe. NewPointe Volunteers are making a difference all across Northeast Ohio!";
+            string teamTerm = "team";
+
+            GroupMemberService groupMemberService = new GroupMemberService(rockContext);
+
+            var volunteerGroups = groupMemberService.Queryable().Where(m => m.PersonId == thePerson.Id && m.Group.GroupTypeId == 42).Select(m => m.Group.Name).ToList();
+
+            string joined = string.Join(", ", volunteerGroups);
+
+
+            if (volunteerGroups.Count > 0)
+            {
+                if (volunteerGroups.Count > 1)
+                {
+                    teamTerm = "teams";
+                }
+
+                volunteerGroupsString = String.Format("Thanks for volunteering on the {0} {1}! Your service is making a difference all across Northeast Ohio.", joined, teamTerm);
+            }
+
+            
+            return volunteerGroupsString;
+        }
+
+
+        protected void btnSubmit_OnClick(object sender, EventArgs e)
+        {
+
+            //TODO: Also set partnership year attribute
+
+            pnlOpportunities.Visible = true;
+            pnlSignature.Visible = false;
+
+            AttributeValueService attributeValueService = new AttributeValueService(rockContext);
+            PersonService personService = new PersonService(rockContext);
+
+            List<Guid> personGuidList = new List<Guid>();
+            personGuidList.Add(_activePerson.Guid);
+
+            var p = attributeValueService.GetByAttributeIdAndEntityId(906, _activePerson.Id);
+
+            var personFromService = personService.GetByGuids(personGuidList).FirstOrDefault();
+
+            p.Value = DateTime.Now.ToString();
+
+
+            personFromService.ConnectionStatusValue.Value = "Partner";
+            personFromService.ConnectionStatusValueId = 65;
+
+
+            rockContext.SaveChanges();
+
+
+        }
+    }
+}
